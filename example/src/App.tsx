9f59e40a030b74fd9bf6fbd6d19002cc00d3cb8f
HAKJOON0223 <2hakjoon@gmail.com>
2022-12-18 23:16:53 +0900

    IMPLEMENT - build

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import {
+  MiwonAsyncBoundary,
+  MiwonErrorBoundary,
+  MiwonSuspense,
+  MiwonLazyLender
+} from 'miwon-boundaries'
 function App() {
-  return <>MY APP</>
+  return (
+    <>
+      MY APP
+      <MiwonAsyncBoundary />
+      <MiwonErrorBoundary />
+      <MiwonSuspense />
+      <MiwonLazyLender />
+    </>
+  )
 }
 
 export default App
