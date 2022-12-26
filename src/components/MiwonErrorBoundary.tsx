86b3fbcf0fdc739a007ffcc119bf0903390bee53
HAKJOON0223 <2hakjoon@gmail.com>
2022-12-26 22:44:32 +0900

    CHANGE - Error Boundary rendering

--- src/components/MiwonErrorBoundary.tsx
+++ src/components/MiwonErrorBoundary.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 
 interface ErrorBoundaryProps {
+  fallback?: ReactNode
   children: ReactNode
 }
 interface ErrorBoundaryState {
@@ -13,38 +14,17 @@ class ErrorBoundary extends React.Component<
 > {
   constructor(props: ErrorBoundaryProps) {
     super(props)
-
-    // Define a state variable to track whether is an error or not
     this.state = { hasError: false }
   }
   static getDerivedStateFromError(error: any) {
-    // Update state so the next render will show the fallback UI
-
     return { hasError: true }
   }
-  componentDidCatch(error: any, errorInfo: any) {
-    // You can use your own error logging service here
-    console.log({ error, errorInfo })
-  }
+  componentDidCatch(error: any, errorInfo: any) {}
+
   render() {
-    // Check if the error is thrown
     if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return (
-        <div>
-          <h2>Oops, there is an error!</h2>
-          <button
-            type="button"
-            onClick={() => this.setState({ hasError: false })}
-          >
-            Try again?
-          </button>
-        </div>
-      )
+      return this.props.fallback ? this.props.fallback : <></>
     }
-
-    // Return children components in case of no error
-
     return this.props.children
   }
 }
