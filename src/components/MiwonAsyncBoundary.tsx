35c08d98085a6b0f7b066c10abbc6d0215780f6a
HAKJOON0223 <2hakjoon@gmail.com>
2022-12-26 23:08:40 +0900

    CHANGE - MiwonAsyncBoundary

--- src/components/MiwonAsyncBoundary.tsx
+++ src/components/MiwonAsyncBoundary.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
+import { MiwonErrorBoundary, MiwonSuspense } from '..'
 
-const MiwonAsyncBoundary = () => {
-  return <>MiwonAsyncBoundary</>
+interface MiwonAsyncBoundaryProps {
+  errorFallback: ReactNode
+  suspenseFallback: ReactNode
+  children: ReactNode
+}
+
+const MiwonAsyncBoundary = ({
+  errorFallback,
+  suspenseFallback,
+  children
+}: MiwonAsyncBoundaryProps) => {
+  return (
+    <MiwonErrorBoundary fallback={errorFallback}>
+      <MiwonSuspense fallback={suspenseFallback}>{children}</MiwonSuspense>
+    </MiwonErrorBoundary>
+  )
 }
 
 export default MiwonAsyncBoundary
