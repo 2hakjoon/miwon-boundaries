fd03a8727050719ceea44549372ddc9581677923
HAKJOON0223 <2hakjoon@gmail.com>
2022-12-26 22:57:52 +0900

    CHANGE - Suspense Component

--- src/components/MiwonSuspense.tsx
+++ src/components/MiwonSuspense.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
-const MiwonSuspense = () => {
-  return <>MiwonSuspense</>
+interface SuspenseProps {
+  fallback?: ReactNode
+  children: ReactNode
+}
+interface SuspenseState {
+  promise: Promise<unknown> | null
+}
+
+class MiwonSuspense extends React.Component<SuspenseProps, SuspenseState> {
+  state: SuspenseState
+  constructor(props: SuspenseProps) {
+    super(props)
+    this.state = {
+      promise: null
+    }
+  }
+
+  componentDidCatch(e: unknown) {
+    if (e instanceof Error) {
+      throw e
+    }
+
+    if (e instanceof Promise) {
+      this.setState(
+        {
+          promise: e
+        },
+        () => {
+          e.then(() => {
+            this.setState({ promise: null })
+          })
+        }
+      )
+    } else {
+      throw e
+    }
+  }
+
+  render() {
+    if (this.state.promise) {
+      return this.props.fallback ? this.props.fallback : <></>
+    }
+    return this.props.children
+  }
 }
 
 export default MiwonSuspense
